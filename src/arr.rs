7bb30c87aba9b89410aba886fd31dd41e14a83ca
j6k1 <jinpu@s254.xrea.com>
2022-03-30 06:55:36 +0900

    Fixed wrong return type of Arr2::iter_mut The value returned by the element_size method does not need to be multiplied by the result of T's size_of, so it is not multiplied. Added implementation of Arr3Iter,Arr3IterMut,Arr4Iter,Arr4IterMut

--- src/arr.rs
+++ src/arr.rs
@@ -44,8 +44,50 @@ impl<T,const N1:usize, const N2:usize> Arr2<T,N1,N2> where T: Default {
         Arr2Iter(&*self.arr)
     }
 
-    pub fn iter_mut<'a>(&'a mut self) -> Arr2Iter<'a,T,N2> {
-        Arr2Iter(&mut *self.arr)
+    pub fn iter_mut<'a>(&'a mut self) -> Arr2IterMut<'a,T,N2> {
+        Arr2IterMut(&mut *self.arr)
+    }
+}
+pub struct Arr3<T,const N1:usize, const N2:usize, const N3:usize> where T: Default {
+    arr:Box<[T]>
+}
+impl<T,const N1:usize,const N2:usize,const N3:usize> Arr3<T,N1,N2,N3> where T: Default {
+    pub fn new() -> Arr3<T,N1,N2,N3> {
+        let mut arr = Vec::with_capacity(N1 * N2 * N3);
+        arr.resize_with(N1*N2*N3,Default::default);
+
+        Arr3 {
+            arr:arr.into_boxed_slice()
+        }
+    }
+
+    pub fn iter<'a>(&'a self) -> Arr3Iter<'a,T,N2,N3> {
+        Arr3Iter(&*self.arr)
+    }
+
+    pub fn iter_mut<'a>(&'a mut self) -> Arr3IterMut<'a,T,N2,N3> {
+        Arr3IterMut(&mut *self.arr)
+    }
+}
+pub struct Arr4<T,const N1:usize, const N2:usize, const N3:usize, const N4:usize> where T: Default {
+    arr:Box<[T]>
+}
+impl<T,const N1:usize,const N2:usize,const N3:usize, const N4:usize> Arr4<T,N1,N2,N3,N4> where T: Default {
+    pub fn new() -> Arr3<T,N1,N2,N3> {
+        let mut arr = Vec::with_capacity(N1 * N2 * N3 * N4);
+        arr.resize_with(N1*N2*N3*N4,Default::default);
+
+        Arr3 {
+            arr:arr.into_boxed_slice()
+        }
+    }
+
+    pub fn iter<'a>(&'a self) -> Arr4Iter<'a,T,N2,N3,N4> {
+        Arr4Iter(&*self.arr)
+    }
+
+    pub fn iter_mut<'a>(&'a mut self) -> Arr4IterMut<'a,T,N2,N3,N4> {
+        Arr4IterMut(&mut *self.arr)
     }
 }
 pub struct ArrView<'a,T,const N:usize> {
@@ -75,7 +117,7 @@ pub struct Arr2Iter<'a,T,const N:usize>(&'a [T]);
 
 impl<'a,T,const N:usize> Arr2Iter<'a,T,N> {
     const fn element_size(&self) -> usize {
-        N * std::mem::size_of::<T>()
+        N
     }
 }
 impl<'a,T,const N:usize> Iterator for Arr2Iter<'a,T,N> {
@@ -100,7 +142,7 @@ pub struct Arr2IterMut<'a,T,const N:usize>(&'a mut [T]);
 
 impl<'a,T,const N:usize> Arr2IterMut<'a,T,N> {
     const fn element_size(&self) -> usize {
-        N * std::mem::size_of::<T>()
+        N
     }
 }
 impl<'a,T,const N:usize> Iterator for Arr2IterMut<'a,T,N> {
@@ -118,6 +160,98 @@ impl<'a,T,const N:usize> Iterator for Arr2IterMut<'a,T,N> {
             Some(ArrViewMut {
                 arr:l
             })
+        }
+    }
+}
+pub struct Arr3Iter<'a,T,const N1:usize,const N2:usize>(&'a [T]);
+
+impl<'a,T,const N1:usize,const N2:usize> Arr3Iter<'a,T,N1,N2> {
+    const fn element_size(&self) -> usize {
+        N1 * N2
+    }
+}
+impl<'a,T,const N1:usize,const N2:usize> Iterator for Arr3Iter<'a,T,N1,N2> {
+    type Item = Arr2Iter<'a,T,N2>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut slice = std::mem::replace(&mut self.0, &mut []);
+        if slice.is_empty() {
+            None
+        } else {
+            let (l,r) = slice.split_at(self.element_size());
+
+            self.0 = r;
+
+            Some(Arr2Iter(l))
+        }
+    }
+}
+pub struct Arr3IterMut<'a,T,const N1:usize,const N2:usize>(&'a mut [T]);
+
+impl<'a,T,const N1:usize,const N2:usize> Arr3IterMut<'a,T,N1,N2> {
+    const fn element_size(&self) -> usize {
+        N1 * N2
+    }
+}
+impl<'a,T,const N1:usize,const N2:usize> Iterator for Arr3IterMut<'a,T,N1,N2> {
+    type Item = Arr2IterMut<'a,T,N2>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut slice = std::mem::replace(&mut self.0, &mut []);
+        if slice.is_empty() {
+            None
+        } else {
+            let (l,r) = slice.split_at_mut(self.element_size());
+
+            self.0 = r;
+
+            Some(Arr2IterMut(l))
+        }
+    }
+}
+pub struct Arr4Iter<'a,T,const N1:usize,const N2:usize,const N3:usize>(&'a [T]);
+
+impl<'a,T,const N1:usize,const N2:usize,const N3:usize> Arr4Iter<'a,T,N1,N2,N3> {
+    const fn element_size(&self) -> usize {
+        N1 * N2 * N3
+    }
+}
+impl<'a,T,const N1:usize,const N2:usize,const N3:usize> Iterator for Arr4Iter<'a,T,N1,N2,N3> {
+    type Item = Arr3Iter<'a,T,N2,N3>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut slice = std::mem::replace(&mut self.0, &mut []);
+        if slice.is_empty() {
+            None
+        } else {
+            let (l,r) = slice.split_at(self.element_size());
+
+            self.0 = r;
+
+            Some(Arr3Iter(l))
+        }
+    }
+}
+pub struct Arr4IterMut<'a,T,const N1:usize,const N2:usize, const N3:usize>(&'a mut [T]);
+
+impl<'a,T,const N1:usize,const N2:usize,const N3:usize> Arr4IterMut<'a,T,N1,N2,N3> {
+    const fn element_size(&self) -> usize {
+        N1 * N2 * N3
+    }
+}
+impl<'a,T,const N1:usize,const N2:usize,const N3:usize> Iterator for Arr4IterMut<'a,T,N1,N2,N3> {
+    type Item = Arr3IterMut<'a,T,N2,N3>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut slice = std::mem::replace(&mut self.0, &mut []);
+        if slice.is_empty() {
+            None
+        } else {
+            let (l,r) = slice.split_at_mut(self.element_size());
+
+            self.0 = r;
+
+            Some(Arr3IterMut(l))
         }
     }
 }

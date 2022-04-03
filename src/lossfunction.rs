1b7fd898225f4475e9850e10bc8987383aac63e2
j6k1 <jinpu@s254.xrea.com>
2022-04-04 08:43:28 +0900

    Added loss function implementation

--- src/lossfunction.rs
+++ src/lossfunction.rs
@@ -1,6 +1,61 @@
+use std::marker::PhantomData;
 use crate::UnitValue;
 
 pub trait LossFunction<U> where U: Clone + Copy + UnitValue<U> {
     fn derive(&self,r:U,t:U) -> U;
     fn name(&self) -> &'static str;
+}
+pub struct Mse<U> where U: Clone + Copy + UnitValue<U> {
+    u:PhantomData<U>
+}
+impl<U> Mse<U> where U: UnitValue<U> {
+    pub fn new() -> Mse<U> {
+        Mse {
+            u:PhantomData::<U>
+        }
+    }
+}
+impl<U> LossFunction<U> for Mse<U> where U: Clone + Copy + UnitValue<U> {
+    fn derive(&self, r: U, t: U) -> U {
+        r - t
+    }
+    fn name(&self) -> &'static str {
+        "mse"
+    }
+}
+pub struct CrossEntropy<U>  where U: Clone + Copy + UnitValue<U> {
+    u:PhantomData<U>
+}
+impl<U> CrossEntropy<U> where U: Clone + Copy + UnitValue<U> {
+    pub fn new() -> CrossEntropy<U> {
+        CrossEntropy {
+            u:PhantomData::<U>
+        }
+    }
+}
+impl<U> LossFunction<U> for CrossEntropy<U> where U: Clone + Copy + UnitValue<U> {
+    fn derive(&self, r: U, t: U) -> U {
+        (r - t) / (r * (U::one() - r))
+    }
+    fn name(&self) -> &'static str {
+        "crossentropy"
+    }
+}
+pub struct CrossEntropyMulticlass<U> where U: Clone + Copy + UnitValue<U> {
+    u:PhantomData<U>
+}
+impl<U> CrossEntropyMulticlass<U> where U: Clone + Copy + UnitValue<U>{
+    pub fn new() -> CrossEntropyMulticlass<U> {
+        CrossEntropyMulticlass {
+            u:PhantomData::<U>
+        }
+    }
+}
+impl<U> LossFunction<U> for CrossEntropyMulticlass<U> where U: Clone + Copy + UnitValue<U> {
+    fn derive(&self, r: U, t: U) -> U {
+        -t / r
+    }
+    fn name(&self) -> &'static str {
+        "crossentropymulticlass"
+    }
 }

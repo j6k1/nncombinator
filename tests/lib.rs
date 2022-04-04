7d224b8eab2c2a95cdcbaa343b47632f63fc69cc
j6k1 <jinpu@s254.xrea.com>
2022-04-04 09:22:41 +0900

    Implemented a test with mnist dataset halfway through.

--- tests/lib.rs
+++ tests/lib.rs
@@ -5,16 +5,22 @@ extern crate rand_xorshift;
 extern crate statrs;
 
 use std::cell::RefCell;
+use std::fs;
+use std::fs::File;
+use std::io::{BufReader, Read};
 use std::ops::DerefMut;
+use std::path::{Path, PathBuf};
 use std::rc::Rc;
 use rand::{prelude, Rng, SeedableRng};
-use rand::prelude::Distribution;
+use rand::prelude::{Distribution, SliceRandom};
 use rand_distr::Normal;
 use rand_xorshift::XorShiftRng;
 use nncombinator::activation::{ReLu, SoftMax};
 use nncombinator::arr::Arr;
 use nncombinator::device::DeviceCpu;
-use nncombinator::layer::{ActivationLayer, AddLayer, AddLayerTrain, InputLayer, LinearLayer, LinearOutputLayer};
+use nncombinator::layer::{ActivationLayer, AddLayer, AddLayerTrain, ForwardAll, InputLayer, LinearLayer, LinearOutputLayer, Train};
+use nncombinator::lossfunction::CrossEntropyMulticlass;
+use nncombinator::optimizer::SGD;
 
 #[test]
 fn test_mnist() {
@@ -30,7 +36,7 @@ fn test_mnist() {
 
     let rnd = rnd_base.clone();
 
-    let net = net.add_layer(|l| {
+    let mut net = net.add_layer(|l| {
         let rnd = rnd.clone();
         LinearLayer::<_,_,_,_,{ 28*28 },64>::new(l,&device, move || n1.sample(&mut rnd.borrow_mut().deref_mut()), || 0.)
     }).add_layer(|l| {
@@ -43,4 +49,43 @@ fn test_mnist() {
     }).add_layer_train(|l| {
         LinearOutputLayer::new(l,&device)
     });
+
+    let mut teachers:Vec<(usize,PathBuf)> = Vec::new();
+
+    for n in 0..10 {
+        for entry in fs::read_dir(Path::new("mnist")
+                                                    .join("mnist_png")
+                                                    .join("training")
+                                                    .join(n.to_string())).unwrap() {
+            let path = entry.unwrap().path();
+
+            teachers.push((n,path));
+        }
+    }
+
+    let mut rng = rand::thread_rng();
+    teachers.shuffle(&mut rng);
+
+    for (n,path) in teachers.iter().take(10000) {
+        for b in BufReader::new(File::open(path).unwrap()).bytes() {
+            let n = *n;
+
+            let pixels = b.iter().map(|&b| f32::from_bits(b as u32)).take(784).collect::<Vec<f32>>();
+
+            let mut input = Arr::<f32,784>::new();
+
+            for (it,p) in input.iter_mut().zip(pixels.iter()) {
+                *it = *p;
+            }
+
+            let mut expected = Arr::new();
+
+            expected[n as usize] = 1.0;
+
+            let mut optimizer = SGD::new(0.001);
+            let lossf = CrossEntropyMulticlass::new();
+
+            net.train(expected,input,&mut optimizer,&lossf);
+        }
+    }
 }

317a6ba2b47238b2cb1caa28f72fe656a62fef38
j6k1 <jinpu@s254.xrea.com>
2022-04-06 07:02:59 +0900

    Added test cases for regression analysis. Added data for testing.

--- tests/lib.rs
+++ tests/lib.rs
@@ -7,7 +7,7 @@ extern crate statrs;
 use std::cell::RefCell;
 use std::fs;
 use std::fs::File;
-use std::io::{BufReader, Read};
+use std::io::{BufRead, BufReader, Read};
 use std::ops::DerefMut;
 use std::path::{Path, PathBuf};
 use std::rc::Rc;
@@ -15,11 +15,11 @@ use rand::{prelude, Rng, SeedableRng};
 use rand::prelude::{Distribution, SliceRandom};
 use rand_distr::Normal;
 use rand_xorshift::XorShiftRng;
-use nncombinator::activation::{ReLu, SoftMax};
+use nncombinator::activation::{ReLu, Sigmoid, SoftMax};
 use nncombinator::arr::Arr;
 use nncombinator::device::DeviceCpu;
 use nncombinator::layer::{ActivationLayer, AddLayer, AddLayerTrain, ForwardAll, InputLayer, LinearLayer, LinearOutputLayer, Train};
-use nncombinator::lossfunction::CrossEntropyMulticlass;
+use nncombinator::lossfunction::{CrossEntropy, CrossEntropyMulticlass};
 use nncombinator::optimizer::{MomentumSGD};
 
 #[test]
@@ -146,4 +146,172 @@ fn test_mnist() {
     println!("correct_answers = {}",correct_answers);
 
     debug_assert!(correct_answers > 10)
+}
+
+
+#[test]
+fn test_weather() {
+    let mut rnd = prelude::thread_rng();
+    let rnd_base = Rc::new(RefCell::new(XorShiftRng::from_seed(rnd.gen())));
+
+    let n1 = Normal::<f32>::new(0.0, 1f32/(2f32/(28f32*28f32)).sqrt()).unwrap();
+    let n2 = Normal::<f32>::new(0.0, 1f32/(28f32*28f32).sqrt()).unwrap();
+
+    let device = DeviceCpu::new();
+
+    let net:InputLayer<f32,Arr<f32,37>> = InputLayer::new();
+
+    let rnd = rnd_base.clone();
+
+    let mut net = net.add_layer(|l| {
+        let rnd = rnd.clone();
+        LinearLayer::<_,_,_,_,37,100>::new(l,&device, move || n1.sample(&mut rnd.borrow_mut().deref_mut()), || 0.)
+    }).add_layer(|l| {
+        ActivationLayer::new(l,ReLu::new(&device),&device)
+    }).add_layer(|l| {
+        let rnd = rnd.clone();
+        LinearLayer::<_,_,_,_,100,100>::new(l,&device, move || n2.sample(&mut rnd.borrow_mut().deref_mut()), || 0.)
+    }).add_layer(|l| {
+        let rnd = rnd.clone();
+        LinearLayer::<_,_,_,_,100,1>::new(l,&device, move || n1.sample(&mut rnd.borrow_mut().deref_mut()), || 0.)
+    }).add_layer(|l| {
+        ActivationLayer::new(l,ReLu::new(&device),&device)
+    }).add_layer(|l| {
+        ActivationLayer::new(l,Sigmoid::new(&device),&device)
+    }).add_layer_train(|l| {
+        LinearOutputLayer::new(l,&device)
+    });
+
+    let mut teachers:Vec<(bool,Vec<f32>)> = Vec::new();
+
+    let mut reader =  BufReader::new(
+                                File::open(Path::new("data")
+                                    .join("weather")
+                                    .join("training")
+                                    .join("weather.csv")).unwrap());
+
+    let mut line:String = String::new();
+
+    loop {
+        if reader.read_line(&mut line).unwrap() == 0 {
+            break;
+        }
+
+        let columns = line.trim().split(',').map(|c| c.to_string()).collect::<Vec<String>>();
+
+        line.clear();
+
+        if columns.len() != 39 {
+            continue;
+        }
+
+        let t = match &*columns[1] {
+            "晴" => true,
+            _ => false
+        };
+
+        let columns = columns.iter().skip(2)
+            .filter(|c| !c.parse::<f32>().is_err())
+            .map(|c| c.parse::<f32>().unwrap())
+            .collect::<Vec<f32>>();
+        if columns.len() < 37 {
+            continue;
+        }
+
+        teachers.push((t,columns));
+    }
+
+    let mut optimizer = MomentumSGD::with_params(0.001,0.9,0.0);
+
+    let mut rng = rand::thread_rng();
+
+    teachers.shuffle(&mut rng);
+
+    let mut correct_answers = 0;
+
+    for _ in 0..10 {
+        teachers.shuffle(&mut rng);
+
+        for (t, columns) in teachers.iter() {
+            let t = *t;
+
+            let mut input = Arr::<f32, 37>::new();
+
+            for (it, p) in input.iter_mut().zip(columns.iter()) {
+                *it = *p;
+            }
+
+            let mut expected = Arr::new();
+
+            expected[0] = if t {
+                1.
+            } else {
+                0.
+            };
+
+            let lossf = CrossEntropy::new();
+
+            net.train(expected, input, &mut optimizer, &lossf);
+        }
+    }
+
+    let mut tests:Vec<(bool,Vec<f32>)> = Vec::new();
+
+    let mut reader =  BufReader::new(
+        File::open(Path::new("data")
+            .join("weather")
+            .join("testing")
+            .join("weather.csv")).unwrap());
+
+    let mut line:String = String::new();
+
+    loop {
+        if reader.read_line(&mut line).unwrap() == 0 {
+            break;
+        }
+
+        let columns = line.trim().split(',').map(|c| c.to_string()).collect::<Vec<String>>();
+
+        line.clear();
+
+        if columns.len() != 39 {
+            continue;
+        }
+
+        let t = match &*columns[1] {
+            "晴" => true,
+            _ => false
+        };
+
+        let columns = columns.iter().skip(2)
+                                        .filter(|c| !c.parse::<f32>().is_err())
+                                        .map(|c| c.parse::<f32>().unwrap())
+                                        .collect::<Vec<f32>>();
+        if columns.len() < 37 {
+            continue;
+        }
+
+        tests.push((t,columns));
+    }
+
+    for (t, columns) in tests.iter() {
+        let t = *t;
+
+        let mut input = Arr::<f32, 37>::new();
+
+        for (it, p) in input.iter_mut().zip(columns.iter()) {
+            *it = *p;
+        }
+
+        let r = net.forward_all(input);
+
+        if (t && r[0] >= 0.9) || r[0] < 0.01 {
+            correct_answers += 1;
+            println!("correct answer!");
+        } else {
+            println!("incorrect. t = {} r = {}",t,r[0]);
+        }
+    }
+
+    println!("correct_answers = {}",correct_answers);
 }

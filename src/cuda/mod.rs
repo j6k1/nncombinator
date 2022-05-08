3d22636646bd3c58ca547c9883f7a3b51362b5ca
j6k1 <jinpu@s254.xrea.com>
2022-05-09 07:26:04 +0900

    Feature Additions Added CudaMemoryPoolPtr implementation.

--- src/cuda/mod.rs
+++ src/cuda/mod.rs
@@ -1,5 +1,7 @@
 use std::fmt::Debug;
+use std::sync::{Arc, Mutex};
 use rcudnn_sys::{cudaMemcpyKind, cudaStream_t};
+use crate::cuda::mem::MemoryPool;
 use crate::error::CudaError;
 
 pub mod ffi;
@@ -133,7 +135,6 @@ impl<T> AsMutPtr<T> for CudaPtr<T> {
         self.ptr
     }
 }
-unsafe impl<T> Send for CudaPtr<T> where T: Send {}
 pub struct CudaHostPtr<T> {
     ptr:*mut T,
     size:usize,
@@ -247,4 +248,101 @@ impl<T> AsVoidMutPtr for CudaHostPtr<T> {
         self.ptr as *mut libc::c_void
     }
 }
-unsafe impl<T> Send for CudaHostPtr<T> where T: Send {}
+pub struct CudaMemoryPoolPtr<T> {
+    ptr:*mut T,
+    size:usize,
+    memory_pool:Arc<Mutex<MemoryPool>>
+}
+impl<T> CudaMemoryPoolPtr<T> {
+    pub fn new(size: usize,memory_pool:&Arc<Mutex<MemoryPool>>) -> Result<CudaMemoryPoolPtr<T>, CudaError> {
+        let ptr:* mut T = match memory_pool.lock() {
+            Ok(mut memory_pool) => {
+                memory_pool.alloc_device(size)?
+            },
+            Err(_) => {
+                return Err(CudaError::InvalidState(String::from(
+                    "Failed to secure exclusive lock on memory pool."
+                )));
+            }
+        };
+
+        Ok(CudaMemoryPoolPtr {
+            ptr: ptr,
+            size: size,
+            memory_pool:Arc::clone(memory_pool)
+        })
+    }
+}
+impl<T: Default + Debug> Memory<T> for CudaMemoryPoolPtr<T> {
+    fn memcpy(&mut self, p:*const T,len:usize) -> Result<usize,rcudnn::Error> {
+        ffi::memcpy(self.ptr,
+                    p,
+                    len,
+                    cudaMemcpyKind::cudaMemcpyHostToDevice)?;
+        Ok(len)
+    }
+
+    fn read_to_vec(&mut self) -> Result<Vec<T>,rcudnn::Error> {
+        let mut r = Vec::with_capacity(self.size);
+        r.resize_with(self.size,Default::default);
+
+        ffi::memcpy(r.as_mut_ptr(),
+                    self.ptr,
+                    self.size,
+                    cudaMemcpyKind::cudaMemcpyDeviceToHost)?;
+        Ok(r)
+    }
+
+    fn read_to_vec_with_size(&mut self,size:usize) -> Result<Vec<T>,rcudnn::Error> {
+        let mut r = Vec::with_capacity(size);
+        r.resize_with(size,Default::default);
+
+        ffi::memcpy(r.as_mut_ptr(),
+                    self.ptr,
+                    size,
+                    cudaMemcpyKind::cudaMemcpyDeviceToHost)?;
+        Ok(r)
+    }
+}
+impl<T> Drop for CudaMemoryPoolPtr<T> {
+    fn drop(&mut self) {
+        match self.memory_pool.lock() {
+            Ok(mut memory_pool) => {
+                memory_pool.deallocate(self.ptr).unwrap()
+            },
+            Err(_) => {
+                panic!("Failed to secure exclusive lock on memory pool.");
+            }
+        }
+    }
+}
+impl<T> AsDoubleVoidPtr for CudaMemoryPoolPtr<T> {
+    fn as_double_void_ptr(&self) -> *const libc::c_void {
+        &self.ptr as *const *mut T as *const libc::c_void
+    }
+}
+impl<T> AsDoubleVoidMutPtr for CudaMemoryPoolPtr<T> {
+    fn as_double_void_mut_ptr(&mut self) -> *mut libc::c_void {
+        &mut self.ptr as *mut *mut T as *mut libc::c_void
+    }
+}
+impl<T> AsVoidPtr for CudaMemoryPoolPtr<T> {
+    fn as_void_ptr(&self) -> *const libc::c_void {
+        self.ptr as *const libc::c_void
+    }
+}
+impl<T> AsVoidMutPtr for CudaMemoryPoolPtr<T> {
+    fn as_void_mut_ptr(&mut self) -> *mut libc::c_void {
+        self.ptr as *mut libc::c_void
+    }
+}
+impl<T> AsPtr<T> for CudaMemoryPoolPtr<T> {
+    fn as_ptr(&self) -> *const T {
+        self.ptr as *const T
+    }
+}
+impl<T> AsMutPtr<T> for CudaMemoryPoolPtr<T> {
+    fn as_mut_ptr(&mut self) -> *mut T {
+        self.ptr
+    }
+}
